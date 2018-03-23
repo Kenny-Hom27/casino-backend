18efe48a285575783713e15b69972f08322fe753
github username <github email address>
2018-03-23 12:57:20 -0400

    added more user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :username, uniqueness: true
+  validates :username, uniqueness: true, presence: true
+  validates :username, length: { maximum: 20}
+  validates :username, length: { minimum: 3}
 end
