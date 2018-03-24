3009f1ba5322fd926df7c29d081baf4deaeeee9e
github username <github email address>
2018-03-24 11:10:38 -0400

    made usernames longer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   validates :username, uniqueness: true, presence: true
-  validates :username, length: { maximum: 10}
+  validates :username, length: { maximum: 15}
   validates :username, length: { minimum: 3}
 end
